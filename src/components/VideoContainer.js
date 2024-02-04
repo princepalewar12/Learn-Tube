b98fb54739c30ad9f81fec82bd81bfad8db4650f
Prince_Palewar <princepalewar12@gmail.com>
2024-02-04 10:09:44 +0530

    create video container and video cards

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEOS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return (
-    <div>VideoContainer</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
 
-export default VideoContainer
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    const videoData = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await videoData.json();
+    setVideos(json.items);
+  };
+
+  return (
+    <div className="flex flex-wrap">
+      {videos.map((video) => (
+        <VideoCard key={video.id} info={video} />
+      ))}
+    </div>
+  );
+};
+
+export default VideoContainer;
